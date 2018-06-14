ca041d2f28ac8ffe59bed085f28c6a6c85c35eeb
Royals Central <danny.zamora@gmail.com>
2018-06-14 11:30:33 -0500

    finished game

---
+++ assets/javascript/app.js
@@ -0,0 +1,195 @@
+$(document).ready(function() {
+    // console.log( "ready!" );
+
+    // track which question we are on
+    var questionCounter = 0;
+    // initial time of 15 seconds for each question
+    var time = 15;
+    // will keep tally of right guesses for end game
+    var correctGuesses = 0;
+    //will keep tally of wrong guesses for end game
+    var incorrectGuesses = 0;
+
+    // question & answer array
+    var questions = [
+      {
+	    question: "What is the best selling album of all time?",
+	    choices: ["Back in Black","Abbey Road","The Dark Side of the Moon","Thriller"],
+	    correctAnswer: "Thriller",
+	    image: "<img src='assets/images/thriller.jpg' class='img-circle shadow'>"
+	  }, 
+	  {
+	    question: "Which country does the band Rammstein hail from?",
+	    choices: ["Austria", "Germany", "Armenia", "Belgium"],
+	    correctAnswer: "Germany",
+	    image: "<img src='assets/images/rammstein.jpg' class='img-circle shadow'>"
+	  }, 
+	  {
+	    question: "Who is the lead singer of the British pop rock band Coldplay?",
+	    choices: ["Jonny Buckland", "Guy Berryman", "Chris Martin", "Will Champion"],
+	    correctAnswer: "Chris Martin",
+	    image: "<img src='assets/images/chrisMartin.jpg' class='img-circle shadow'>"
+	  }, 
+	  {
+	    question: "Which one of these songs did the group &quot;Men At Work&quot; NOT make?",
+	    choices: ["Who Can It Be Now?", "Down Under", "It&#039;s a Mistake", "Safety Dance"],
+	    correctAnswer: "Safety Dance",
+	    image: "<img src='assets/images/menAtWork.jpg' class='img-circle shadow'>"
+	  }, 
+	  {
+	    question: "Which band recorded the album &quot;Parallel Lines&quot;?",
+	    choices: ["Paramore", "The Police", "Blondie", "Coldplay"],
+	    correctAnswer: "Blondie",
+	    image: "<img src='assets/images/blondie.jpg' class='img-circle shadow'>"
+      }];
+	  
+
+	// create question contents according to question count
+	function questionContent() {
+		// a for loop would be cool here...
+    	$("#gameScreen").append("<p><strong>" + 
+    		questions[questionCounter].question + 
+    		"</p><p class='choices'>" + 
+    		questions[questionCounter].choices[0] + 
+    		"</p><p class='choices'>" + 
+    		questions[questionCounter].choices[1] + 
+    		"</p><p class='choices'>" + 
+    		questions[questionCounter].choices[2] + 
+    		"</p><p class='choices'>" + 
+    		questions[questionCounter].choices[3] + 
+    		"</strong></p>");
+	}
+
+	// user guessed correctly
+	function userWin() {
+		$("#gameScreen").html("<p>You got it right!</p>");
+		correctGuesses++;
+		var correctAnswer = questions[questionCounter].correctAnswer;
+		$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
+			correctAnswer + 
+			"</span></p>" + 
+			questions[questionCounter].image);
+		setTimeout(nextQuestion, 4000);
+		questionCounter++;
+	}
+
+	// user guessed incorrectly
+	function userLoss() {
+		$("#gameScreen").html("<p>Nope, that's not it!</p>");
+		incorrectGuesses++;
+		var correctAnswer = questions[questionCounter].correctAnswer;
+		$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
+			correctAnswer + 
+			"</span></p>" + 
+			questions[questionCounter].image);
+		setTimeout(nextQuestion, 4000);
+		questionCounter++;
+	}
+
+	// user ran out of time
+	function userTimeout() {
+		if (time === 0) {
+			$("#gameScreen").html("<p>You ran out of time!</p>");
+			incorrectGuesses++;
+			var correctAnswer = questions[questionCounter].correctAnswer;
+			$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
+				correctAnswer + 
+				"</span></p>" + 
+				questions[questionCounter].image);
+			setTimeout(nextQuestion, 2000);
+			questionCounter++;
+		}
+	}
+
+	// screen that shows final score and nice message :)
+	function resultsScreen() {
+		if (correctGuesses === questions.length) {
+			var endMessage = "Perfection! Might want to go outside more tho";
+			var bottomText = "#howdidyouknowthat?";
+		}
+		else if (correctGuesses > incorrectGuesses) {
+			var endMessage = "Not bad noob";
+			var bottomText = "#thesequestionsareweird";
+		}
+		else {
+			var endMessage = "Better Luck Next Time...";
+			var bottomText = "#scrub";
+		}
+		$("#gameScreen").html("<p>" + endMessage + "</p>" + "<p>You got <strong>" + 
+			correctGuesses + "</strong> right.</p>" + 
+			"<p>You got <strong>" + incorrectGuesses + "</strong> wrong.</p>");
+		$("#gameScreen").append("<h1 id='start'>Start Over?</h1>");
+		$("#bottomText").html(bottomText);
+		gameReset();
+		$("#start").click(nextQuestion);
+	}
+
+	// game clock currently set to 15 seconds
+	function timer() {
+		clock = setInterval(countDown, 1000);
+		function countDown() {
+			if (time < 1) {
+				clearInterval(clock);
+				userTimeout();
+			}
+			if (time > 0) {
+				time--;
+			}
+			$("#timer").html("<strong>" + time + "</strong>");
+		}
+	}
+
+	// moves question counter forward to show next question
+	function nextQuestion() {
+		if (questionCounter < questions.length) {
+			time = 15;
+			$("#gameScreen").html("<p>You have <span id='timer'>" + time + "</span> seconds left!</p>");
+			questionContent();
+			timer();
+			userTimeout();
+		}
+		else {
+			resultsScreen();
+		}
+	// console.log(questionCounter);
+	// console.log(questions[questionCounter].correctAnswer);
+	}
+
+	// reset score and counter parameters on restart
+	function gameReset() {
+		questionCounter = 0;
+		correctGuesses = 0;
+		incorrectGuesses = 0;
+	}
+
+    function startGame() {
+    	$("#gameScreen").html("<p>You have <span id='timer'>" + time + "</span> seconds left!</p>");
+    	$("#start").hide();
+    	// $("#gameScreen").append("<div id='question'>");
+    	// var nextQuestion = questionContent(questionCounter);
+    	// $("#gameScreen").append(nextQuestion);
+
+		// $("#gameScreen").append("<p>" + questions[questionCounter].question + "</p><p>" + questions[questionCounter].choices[0] + "</p><p>" + questions[questionCounter].choices[1] + "</p><p>" + questions[questionCounter].choices[2] + "</p><p>" + questions[questionCounter].choices[3] + "</p>");
+		// questionCounter++;
+		questionContent();
+    	timer();
+    	userTimeout();
+    }
+
+    // this starts the game
+    $("#start").click(nextQuestion);
+
+    // click function to trigger right or wrong screen
+	$("#gameScreen").on("click", ".choices", (function() {
+		// alert("clicked!");
+		var userGuess = $(this).text();
+		if (userGuess === questions[questionCounter].correctAnswer) {
+			clearInterval(clock);
+			userWin();
+		}
+		else {
+			clearInterval(clock);
+			userLoss();
+		}
+	}));
+});
